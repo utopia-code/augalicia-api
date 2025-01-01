9b4f9d0b72482f1be381b657c0d36cf989b35775
utopia-code <utopiacode.developer@gmail.com>
2025-01-01 17:28:06 +0100

    AuGalicia API: Render config: add configModule as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -15,6 +16,9 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
     TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
     UsersModule, 
     ProductsModule, 
