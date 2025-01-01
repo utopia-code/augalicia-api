ee0fa6612465048430d60ad86047a3a12ba866db
utopia-code <utopiacode.developer@gmail.com>
2025-01-01 21:34:22 +0100

    AuGalicia API: Render config: test service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Está funcinando la base de datos de AuGalicia';
   }
 }
