163a4696c67ce1f2faf1632f0bed9a647b4668d6
utopia-code <utopiacode.developer@gmail.com>
2025-01-01 17:29:00 +0100

    AuGalicia API: Render config: add JWT_SECRET in auth.module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,31 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-import { UsersModule } from 'src/users/users.module';
+import { UsersModule } from './../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { jwtConstants } from './constants/jwt.constants';
+// import { jwtConstants } from './constants/jwt.constants';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '1d' },
+    // JwtModule.register({
+    //   global: true,
+      // secret: jwtConstants.secret,
+    //   signOptions: { expiresIn: '1d' },
+    // }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>("JWT_SECRET"),
+        signOptions: { expiresIn: "1d" },
+        global: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
+  exports: [JwtModule],
 })
 export class AuthModule {}
